ae39bf943ff624aba8370321ea45ef636f0aaacb
juanescardona <juanescardona@gmail.com>
2013-10-20 12:53:15 -0500

    Coloqué el proyecto en el repositorio

---
+++ ConversionRomanos/Test/TestConversionRomanos.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestConversionRomanos {
+
+	@Before
+	public void init(){
+		
+	}
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
